3d219fd3e61eb5de127d6bc52500b0cc3f6921d5
piyushjain <jainpiyush26@gmail.com>
2018-06-04 22:57:55 -0400

    adding the docs file and changes to the file name

--- package/taskgoblin.bat
+++ package/taskgoblin.bat
@@ -1,6 +1,5 @@
 @echo off
 SET current_directory=%~dp0
-SET ver=003
 cd %current_directory%
 cd ..
-python bin/task_goblin.%ver%.py
+python bin/task_goblin.py
--- package/taskgoblin.sh
+++ package/taskgoblin.sh
@@ -1,5 +1,4 @@
  #! /bin/sh
- export ver 003
  cd $PWD
  cd ..
- python bin/taskgoblin.$ver.py
+ python bin/taskgoblin.py
